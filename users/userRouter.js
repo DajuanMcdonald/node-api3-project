b06ac30733c640ee832c3c06fec71720865e99bb
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-16 19:50:24 -0400

    added validateUser logic

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
+const Users = require('./userDb');
+const Posts = require('../posts/postDb');
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUser, (req, res) => {
+  Users.insert(req.body)
+  .then(user => { res.status(200).json(user)})
+  .catch(err => res.status(500).json({message: }))
 });
 
 router.get('/', (req, res) => {
@@ -38,10 +41,20 @@ function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (req.body) {
+    if (req.body.name) {
+      next();
+    } else {
+      res.status(400).json({message: "Missing user data"})
+    }
+  } else {
+    res.status(400).json({message: "Missing user data"})
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  
 }
 
 module.exports = router;
