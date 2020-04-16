c4756a4356260375288902ce7c6210d48a626497
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-16 19:40:25 -0400

    added console log of req.url and req.method to server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ server.use(logger)
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get("Origin")}`);
+  next();
+}
 
 module.exports = server;
